233bd953e73e92aa0d29e7538849b18f0a789891
DraconDev <dracsharp@gmail.com>
2026-09-21 14:49:23 +0100

    1 file(s) in extensions/goal-commands.ts DELTA:+1/-0 | EXT:ts

--- extensions/goal-commands.ts
+++ extensions/goal-commands.ts
@@ -2889,6 +2889,7 @@ async function cmdSettings(args: string, ctx: ExtensionContext): Promise<void> {
       `auditorToolTimeoutMs: ${((effectiveSettings.auditorToolTimeoutMs ?? DEFAULT_AUDITOR_TOOL_TIMEOUT_MS) / 60000).toString()}m  [${prov.auditorToolTimeoutMs?.source ?? "default"}]`,
       `auditorStallMs: ${((effectiveSettings.auditorStallMs ?? DEFAULT_AUDITOR_STALL_MS) / 60000).toString()}m  [${prov.auditorStallMs?.source ?? "default"}]`,
       `auditJobRetentionMs: ${((effectiveSettings.auditJobRetentionMs ?? AUDIT_JOB_CLEANUP_MIN_AGE_MS) / 60000).toString()}m  [${prov.auditJobRetentionMs?.source ?? "default"}]`,
+      fmt("auditSpotCheckRate", "auditSpotCheckRate"),
       fmt("auditorInspection", "auditorInspection"),
       fmt("hourlyRetryProbe", "hourlyRetryProbe"),
       fmt("subagentModelStrategy", "subagentModelStrategy"),
